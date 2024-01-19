a6132a859c2bacdbf7569429ff79ba6e5cb3ed5d
Med16-11 <singhmedhavi923@gmail.com>
2024-01-19 12:29:39 +0530

    added another security group with two ingress and one egress rule

--- exercise_0100/ec2.tf
+++ exercise_0100/ec2.tf
@@ -26,3 +26,35 @@ resource "aws_security_group" "sec_ssh" {
     Terraform =  true
   }
 }
+
+resource "aws_security_group" "sec_http" {
+  name        = "security group 2 using Terraform"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = "vpc-0365ac2b87223e786"
+
+  ingress{
+    from_port = 8443
+    to_port = 8443
+    protocol = "tcp"
+    cidr_blocks = ["192.168.0.0/16"]
+  }
+
+  ingress{
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["192.168.0.0/16"]
+  }
+
+  egress{
+    from_port = 0
+    to_port = 0
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sec_http"
+    Terraform =  true
+  }
+}
