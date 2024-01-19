89e0b5de5d17158d09049975debc1edbaab52df9
Med16-11 <singhmedhavi923@gmail.com>
2024-01-19 12:04:28 +0530

    security group added with one ingress rule

--- exercise_0100/ec2.tf
+++ exercise_0100/ec2.tf
@@ -7,3 +7,22 @@ resource "aws_instance" "web" {
     Terraform =  true
   }
 }
+
+resource "aws_security_group" "sec_ssh" {
+  name        = "security group using Terraform"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = "vpc-0365ac2b87223e786"
+
+  ingress{
+    description = "TLC from VPC"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sec_ssh"
+    Terraform =  true
+  }
+}
--- exercise_0100/versions.tf
+++ exercise_0100/versions.tf
@@ -1,5 +1,5 @@
 terraform {
-    required_version = "1.0.8"
+    required_version = "1.6.6"
 
     required_providers{
         aws = {
